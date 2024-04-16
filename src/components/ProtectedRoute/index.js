1e349d03fe1d400d81f7bf40e95b405cc331d83c
Saravana1212 <wellsaravana1@gmail.com>
2024-04-16 14:22:37 +0000

    solution

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,13 @@
+// import {Redirect,Router} from 'react-router-dom'
+
+// import Cookies from 'js-cookie'
+
+// const ProtectedRoute =(props)=>{
+//     const jwtToken = Cookies.get('jwt_token')
+//     if (jwtToken === undefined){
+//         return <Redirect to="/ebank/login" />
+//     }
+//     return <Router {...props} />
+// }
+
+// export default ProtectedRoute
